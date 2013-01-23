66c42e6f44371cebf4686761e8c478877fc1c3e3
Stephen Caudill <scaudill@gmail.com>
2013-01-23 13:25:24 -0500

    Highlight active link

--- app.rb
+++ app.rb
@@ -2,6 +2,29 @@ before do
   response.headers['Cache-Control'] = 'public, max-age=604800' if production?
 end
 
+helpers do
+  def active_class_for(uri_path)
+    request.path_info == uri_path ? 'active' : nil
+  end
+
+  def root_path
+    '/'
+  end
+
+  def usage_path
+    '/usage'
+  end
+
+  def advanced_usage_path
+    '/advanced'
+  end
+
+  def contributing_path
+    '/contributing'
+  end
+
+end
+
 get '/stylesheets/*.css' do |file_name|
   sass(file_name.to_sym)
 end
